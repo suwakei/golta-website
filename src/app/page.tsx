c076395e50494e2b32f413da733811e8a351deb8
keitosuwahara <keitosuwahara0816@gmail.com>
2025-11-19 11:48:32 +0900

    added logo on Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import styles from "@/styles/content.module.css";
@@ -8,6 +9,15 @@ export default function Home() {
   return (
     <>
       <div className={styles.container}>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <Image
+            src="/backwhite.svg"
+            alt="Golta Logo"
+            width={500}
+            height={200}
+            priority
+          />
+        </div>
         <ReactMarkdown remarkPlugins={[remarkGfm]}>{HomeContent}</ReactMarkdown>
       </div>
       <Footer />
